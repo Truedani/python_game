ce074a70e999842475ef5e299d8339e9300ecd66
daniel.calaver@gmail.com <calaver@luxonis.com>
2021-03-27 17:14:36 +0200

    broken spells

--- character.py
+++ character.py
@@ -10,31 +10,16 @@ class Character:
 
     
     def check_life(self):
-        if self._hp <= 0:
-            print(f'{self._name} is dead!')
-            return False
-        return True
+        print(f'{self._name} is dead!')
     
     
     def heal(self):
-        luck = random.uniform(0, 1)
-        heal = luck * self._ability_power
         print(f'{self._name} healed for {heal} HP')
-        self._hp += heal
         
 
     def attack(self, target):
-        luck = random.uniform(0, 1)
-        damage = luck * self._ability_power
         print(f'{self._name} attacked with {damage} damage!')
-        target.take_damage(damage)
-        return damage
 
     
     def take_damage(self, damage):
-        luck = random.uniform(0, 1)
-        if luck > 1-self._dodge:
-            print(f'{self._name} dodged!')
-        else:
             print(f'{self._name} took {damage} damage!')
-            self._hp -= damage
