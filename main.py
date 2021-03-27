3d632b479dde38eee69e99aeb270b890cd4a2faa
daniel.calaver@gmail.com <calaver@luxonis.com>
2021-03-27 17:02:56 +0200

    ready to merge

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 from character import Character 
 import random
 
+
 def main():
-    boss = Character(name='Boss', hp=100, ability_power=10, dodge=0.15)
-    protagonist = Character(name='You', hp=100, ability_power=10, dodge=0.15)
-    
-    while True:
-        ability = random.uniform(0, 1)
-        if ability > 0.5:
-            protagonist.attack(boss)
-        else:
-            protagonist.heal()
-        boss.attack(protagonist)
-        
-        boss_status = boss.check_life()
-        protagonist_status = protagonist.check_life()
-        if not boss_status or not protagonist_status:
-            break
-    print('Game over!')
+    pass
 
 
 if __name__ == '__main__':
