fe8b9eb16c704875b55a2f2c8b7802a98974ae64
daniel.calaver@gmail.com <calaver@luxonis.com>
2021-03-27 17:13:21 +0200

    add story

--- character.py
+++ character.py
@@ -1,25 +1,40 @@
+import random
+
 class Character:
-
-
+    
     def __init__(self, name, hp=100, ability_power=10, dodge=0.15):
         self._name          = name
         self._hp            = hp
         self._ability_power = ability_power
         self._dodge         = dodge
-    
 
+    
     def check_life(self):
-        print(f'{self._name} is alive!')
+        if self._hp <= 0:
+            print(f'{self._name} is dead!')
+            return False
         return True
     
     
     def heal(self):
-        print(f'{self._name} is healing')
+        luck = random.uniform(0, 1)
+        heal = luck * self._ability_power
+        print(f'{self._name} healed for {heal} HP')
+        self._hp += heal
         
 
     def attack(self, target):
-        print(f'{self._name} attacked!')
+        luck = random.uniform(0, 1)
+        damage = luck * self._ability_power
+        print(f'{self._name} attacked with {damage} damage!')
+        target.take_damage(damage)
+        return damage
 
     
     def take_damage(self, damage):
-        print(f'{self._name} took damage!')
+        luck = random.uniform(0, 1)
+        if luck > 1-self._dodge:
+            print(f'{self._name} dodged!')
+        else:
+            print(f'{self._name} took {damage} damage!')
+            self._hp -= damage
