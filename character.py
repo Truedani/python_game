5f10f71c27eb30cc766f5e8bd02c92b1d137208b
Daniel Calaver <daniel.calaver@gmail.com>
2021-04-08 16:18:45 +0300

    unstable

--- character.py
+++ character.py
@@ -1,25 +1,27 @@
 import random
 
 class Character:
-    
+   
+
     def __init__(self, name, hp=100, ability_power=10, dodge=0.15):
         self._name          = name
         self._hp            = hp
         self._ability_power = ability_power
         self._dodge         = dodge
+   
 
-    
+    def get_name(self):
+        return self._name
+   
+
     def check_life(self):
         print(f'{self._name} is dead!')
     
     
     def heal(self):
-        print(f'{self._name} healed for {heal} HP')
+        print(f'{self._name} healed')
         
 
     def attack(self, target):
-        print(f'{self._name} attacked with {damage} damage!')
+        print(f'{self._name} attacked with damage!')
 
-    
-    def take_damage(self, damage):
-        print(f'{self._name} took {damage} damage!')
--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from character import Character
 import random
 
 def main():
+    name = input('Enter your name: ')
     boss = Character(name='Boss', hp=100, ability_power=10, dodge=0.15)
-    protagonist = Character(name='You', hp=100, ability_power=10, dodge=0.15)
-    
+    protagonist = Character(name=name, hp=100, ability_power=10, dodge=0.15)
+    print(f'{protagonist.get_name()} entered a dungeon and encounters the first Boss')
+
     while True:
-        ability = random.uniform(0, 1)
-        if ability > 0.5:
+        action = input('Choose action (attack/heal)')
+        if action is 'attack':
             protagonist.attack(boss)
-        else:
+        elif action is 'heal':
             protagonist.heal()
+        else:
+            print(f'{protagonist.get_name()} didn\'t choose heal or attack, he passes the turn')
         boss.attack(protagonist)
         
         boss_status = boss.check_life()
         protagonist_status = protagonist.check_life()
-        if not boss_status or not protagonist_status:
+        if not boss_status:
+            print(f'{boss.get_name()} defeated! {
+            break
+        elif not protagonist_status:
             break
     print('Game over!')
 
